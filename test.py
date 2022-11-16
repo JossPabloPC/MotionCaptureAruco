b33102f3d72beae06e2c3387d363fc2cc6b9ff01
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-16 11:31:09 -0600

    images imported

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+# Importando las bibliotecas necesarias
+from imutils.video import VideoStream
 import matplotlib.pyplot as plt
-import numpy as np
 import argparse
 import imutils
+import time
 import cv2
+import sys
+
+
+# Pasando las direcciones como un argumento
+DIR = '/content/drive/My Drive/Colab Notebooks/UP_Lectures_202X/TIAv/random_Images/'
+
+args = {
+    "image": DIR+"aruco_01.png",
+    "type": "DICT_5X5_100",
+}
