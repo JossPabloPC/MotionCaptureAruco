f433a08625929115846ba03b1fe50de0739a08db
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-11-16 14:39:12 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,66 +1,32 @@
 # Importando las bibliotecas necesarias
 from imutils.video import VideoStream
 import matplotlib.pyplot as plt
-import argparse
 import imutils
-import time
 import cv2
+import numpy as np
 import sys
-
-def plt_imshow(title, image):
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	plt.imshow(image)
-	plt.title(title)
-	plt.grid(False)
-	plt.axis('off')
-	plt.show()
 
 # Pasando las direcciones como un argumento
 DIR = 'C:/Users/pcjos/OneDrive/Documentos/VS/ComputerVision/Sources/'
 
 
-args = {
+parametros = {
     "type": "DICT_5X5_100",
     "video": DIR+"aruco_sample.mp4",
     "output": DIR+"output.avi"
 }
 
-# Definiendo los nombres de cada posible familia de diccionarios de ArUCo válido para OpenCV
+#Diccionario de Arucos
 ARUCO_DICT = {
-	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
 	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
 	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
 	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
 	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
 
-# Vericando que el ArUCo proporcionado sea válido  para OpenCV
-if ARUCO_DICT.get(args["type"], None) is None:
-	print("[INFO] ArUCo tag of '{}' is not supported".format(
-		args["type"]))
-	sys.exit(0)
-
-# Cargando el diccionario de ArUCo y obteniendo sus parámetros
-print("[INFO] detecting '{}' tags...".format(args["type"]))
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-arucoParams = cv2.aruco.DetectorParameters_create() #Obteniendo los parámetros del ArUCo
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[parametros["type"]])
+arucoParams = cv2.aruco.DetectorParameters_create()
 
 
 import imageio
@@ -69,51 +35,21 @@ import matplotlib.animation as animation
 from skimage.transform import resize
 from IPython.display import HTML
 
-def display_video(video):
-    fig = plt.figure(figsize=(3,3))  #Display size specification
 
-    mov = []
-    for i in range(len(video)):  #Append videos one by one to mov
-        img = plt.imshow(video[i], animated=True)
-        plt.axis('off')
-        mov.append([img])
+#Lee el video
+video = imageio.mimread(parametros["video"], memtest= "1000MB")  #Loading video
 
-    #Animation creation
-    anime = animation.ArtistAnimation(fig, mov, interval=50, repeat_delay=1000)
-
-    plt.close()
-    return anime
-
-video = imageio.mimread(args["video"], memtest= "1000MB")  #Loading video
-#video = [resize(frame, (256, 256))[..., :3] for frame in video]    #Size adjustment (if necessary)
-
-# PARA VIDEO PREGRABADO
-
-# Inicializando el video, y apuntando al video de salida
-vs = cv2.VideoCapture(args["video"])
+vs = cv2.VideoCapture(parametros["video"])
 writer = None
-(W, H) = (None, None)
 
-# try to determine the total number of frames in the video file
-try:
-	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
-		else cv2.CAP_PROP_FRAME_COUNT
-	total = int(vs.get(prop))
-	print("[INFO] {} total frames in video".format(total))
-
-# Manejor de excepciones
-except:
-	print("[INFO] could not determine # of frames in video")
-	print("[INFO] no approx. completion time can be provided")
-	total = -1
-
-# Iterando sobre las tramas del video
+# Iterando sobre los cuadros del video
 output = []
+bakedAnim = np.matrix([[],[],[]])
 while True:
-	# leyendo la trama siguiente del video
+	# va leyendo cada frame del video
 	(grabbed, frame) = vs.read()
  
-	# Si no se encontró la trama, salir.
+	#Si no pudo leer le video
 	if not grabbed:
 		break
 
@@ -128,7 +64,7 @@ while True:
 	if len(corners) > 0:
 		# A lista
 		ids = ids.flatten()
-
+		i = 0
 		# Iterando sobre las coordenadas de las esquinas de cada BB
 		for (markerCorner, markerID) in zip(corners, ids):
 			corners = markerCorner.reshape((4, 2))
@@ -150,6 +86,12 @@ while True:
 			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
 			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+ 
+
+			bakedAnim = np.append(bakedAnim, [[cX], [cY], [0]], axis = 1)
+			bakedAnim[i,i] = bakedAnim[i,i] - bakedAnim[0,0] 
+			i = i + 1
+
 
 			# Dibujar el ID
 			cv2.putText(frame, str(markerID),
@@ -161,13 +103,14 @@ while True:
 	if writer is None:
 		# initialize our video writer
 		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+		writer = cv2.VideoWriter(parametros["output"], fourcc, 30,
 			(frame.shape[1], frame.shape[0]), True)
 
 	# Guardando la trama con los ArUCos detectados
 	output.append(frame)
 	writer.write(frame)
 
+print(bakedAnim)
 # Liberando los apuntadores inicializados
 print("[INFO] cleaning up...")
 writer.release()
